7306becf584aa1d44e43db78391cd2a5c1c7973e
pgstrk <seniha.oztemiz@gmail.com>
2015-11-27 17:14:46 +0200

    Update configle.sh

--- configle.sh
+++ configle.sh
@@ -6,7 +6,7 @@ tarih1=`date "+%Y-%m-%d"`
 tarih2=`date "+%m-%Y"`
 mkdir /tftpboot/swconfig/$tarih2
 #List of SW IP's is at the path: /tftpboot/swconfig/canli_cihaz_ip
-for i in \ `cat /tftpboot/ciscoswconfig/canli_cihaz_ip`;
+for i in \ `cat /tftpboot/swconfig/canli_cihaz_ip`;
 do
 /tftpboot/swconfig/ssh-ile-config-topla $i > /tftpboot/swconfig/$tarih2/$tarih1-$i-ssh.conf ;
 sed '1,/Building/ d' /tftpboot/swconfig/$tarih2/$tarih1-$i-ssh.conf >> /tftpboot/swconfig/$tarih2/conf-$i-ssh.conf;
